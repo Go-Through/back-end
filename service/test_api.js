887effe9e90c7c3cd072040e821115c6322f9adf
beoms <bm1478@naver.com>
2020-07-12 18:48:30 +0900

    collect area code

--- service/test_api.js
+++ service/test_api.js
@@ -2,10 +2,8 @@ const axios = require('axios');
 const fs = require('fs');
 
 const url = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService';
-const service = 'areaCode';
 const dataResult = JSON.parse(fs.readFileSync('../service-key.json', 'utf8'));
-
-axios.get(`${url}/${service}`, {
+const baseParams = {
     params: {
         ServiceKey: dataResult.key,
         numOfRows: '100',
@@ -13,8 +11,45 @@ axios.get(`${url}/${service}`, {
         MobileOS: 'ETC',
         MobileApp: 'AppTest',
         _type: 'json',
-        areaCode: '1'
     }
-}).then(function (result) {
-    console.log(result.data);
-});
+};
+
+async function getFullLocation() {
+    const service = 'areaCode';
+    const areaFirsts = [];
+    const areas = [];
+
+    let axiosResult;
+    axiosResult = await axios.get(`${url}/${service}`, baseParams);
+
+    if (axiosResult.data.response.header.resultCode === '0000') {
+        for (const item of axiosResult.data.response.body.items.item) {
+            areaFirsts.push(item);
+            areas.push(item);
+        }
+        console.log(areaFirsts);
+    }
+
+    for (const area of areaFirsts) {
+        baseParams.params.areaCode = area.code;
+        axiosResult = await axios.get(`${url}/${service}`, baseParams);
+        if (axiosResult.data.response.header.resultCode === '0000') {
+            if (!Array.isArray(axiosResult.data.response.body.items.item)) {
+                axiosResult.data.response.body.items.item.firstArea = area.name;
+                areas.push(axiosResult.data.response.body.items.item);
+            } else {
+                for (const item of axiosResult.data.response.body.items.item) {
+                    item.firstArea = area.name;
+                    areas.push(item);
+                }
+            }
+            console.log(areas);
+        }
+        else {
+            console.log(`axios Problem - ${axiosResult.data.response.header}`);
+        }
+    }
+
+}
+
+getFullLocation().then();
