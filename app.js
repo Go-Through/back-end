0b3ca4702f2e6b25e837085dd529a6dc492e2b05
beoms <bm1478@naver.com>
2020-07-23 21:56:56 +0900

    update port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 // CORS 허용
 // app.use(cors);
 /* app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
 }); */
 
 app.use(logger('dev'));
