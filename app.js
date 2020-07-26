c3ec6364f080300a4c490ee1b71ea56eb88c979a
beoms <bm1478@naver.com>
2020-07-26 18:04:51 +0900

    cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // CORS 허용
-app.use(cors);
+app.use(cors());
 /* app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
 }); */
